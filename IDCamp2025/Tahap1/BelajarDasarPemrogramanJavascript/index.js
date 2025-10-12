0f34101927f45a4d5b939d522b0cfaec03cc62b7
Kiki <207105651+rizzman03@users.noreply.github.com>
2025-10-12 12:16:06 +0700

    new folder

---
+++ IDCamp2025/Tahap1/BelajarDasarPemrogramanJavascript/index.js
@@ -0,0 +1,53 @@
+// // const employees = [
+// //   {
+// //     name: 'Fulan',
+// //     email: 'fulan@dicoding.com',
+// //     joinYear: 2020,
+// //   },
+// // ];
+
+// // function addEmployee(name, email, joinYear) {
+// //   /**
+// //    * @TODO
+// //    * lengkapi fungsi ini agar dapat menambahkan objek employee baru
+// //    * berdasarkan nilai argumen fungsi dan simpan ke dalam array `employees`
+// //    */
+// //    const anakbaru = {
+// //     name: name,
+// //     email: email,
+// //     joinYear: joinYear,
+// //    }
+// //    employees.push(anakbaru);
+// //    console.log('After:', employees);
+   
+// // }
+
+// // addEmployee('Fathan', 'gmail.com', 2025);
+
+// // const person = {
+// //   name: 'Fathan',
+// // }
+
+// // const {name} = person;
+// // console.log(name);
+// // console.log(person);
+
+// const arrayKu = [['apple'], ...['banana']];
+
+// console.log(arrayKu);
+
+// const mapKu = new Map(['Saya', 'Fathan']);
+
+// console.log(mapKu.has('name'));
+
+// const date = new  Date().getDay();
+// console.log(date);
+
+let i = 0;
+
+while (i < 5) {
+  console.log(`Angka ke-${i} adalah ${i}.`)
+  i++;
+  
+
+}
