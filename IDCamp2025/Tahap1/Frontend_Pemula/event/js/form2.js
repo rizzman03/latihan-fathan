cb62a02d3fa835cd1535bd83a0c5f69f1c82857a
Kiki <207105651+rizzman03@users.noreply.github.com>
2025-11-08 22:02:37 +0700

    add todoapps project

--- IDCamp2025/Tahap1/Frontend_Pemula/event/js/form2.js
+++ IDCamp2025/Tahap1/Frontend_Pemula/event/js/form2.js
@@ -1,33 +1,61 @@
-const { useInsertionEffect } = require("react");
-
 document.addEventListener("DOMContentLoaded", function () {
   const inputMaxLengthOnLoad = document.getElementById("inputNama").maxLength;
   document.getElementById("sisaKarakter").innerText = inputMaxLengthOnLoad;
-});
 
-document.getElementById("inputNama").addEventListener("input", function () {
-  const jumlahKarakterDiketik =
-    document.getElementById("inputNama").value.length;
-  const jumlahKarakterMaksimal = document.getElementById("inputNama").maxLength;
+  document.getElementById("inputNama").addEventListener("input", function () {
+    const jumlahKarakterDiketik =
+      document.getElementById("inputNama").value.length;
+    const jumlahKarakterMaksimal =
+      document.getElementById("inputNama").maxLength;
 
-  console.log("jumlahKarakterDiketik: ", jumlahKarakterDiketik);
-  console.log("jumlahKarakterMaksimal", jumlahKarakterMaksimal);
-  const sisaKarakterUpdate = jumlahKarakterMaksimal - jumlahKarakterDiketik;
-  document.getElementById("sisaKarakter").innerText =
-    sisaKarakterUpdate.toString();
-
-  if (sisaKarakterUpdate === 0) {
+    console.log("jumlahKarakterDiketik: ", jumlahKarakterDiketik);
+    console.log("jumlahKarakterMaksimal", jumlahKarakterMaksimal);
+    const sisaKarakterUpdate = jumlahKarakterMaksimal - jumlahKarakterDiketik;
     document.getElementById("sisaKarakter").innerText =
-      "Batas maksimal tercapai";
-  } else if (sisaKarakterUpdate <= 5) {
-    document.getElementById("notifikasiSisaKarakter").style.color = "red";
-  } else {
-    document.getElementById("notifikasiSisaKarakter").style.color = "black";
-  }
-});
+      sisaKarakterUpdate.toString();
 
-document.getElementById("inputNama").addEventListener("focus", function () {
-  console.log("inputNama: focus");
-  document.getElementById("notifikasiSisaKarakter").style.visibility =
-    "visible";
+    if (sisaKarakterUpdate === 0) {
+      document.getElementById("sisaKarakter").innerText =
+        "Batas maksimal tercapai";
+    } else if (sisaKarakterUpdate <= 5) {
+      document.getElementById("notifikasiSisaKarakter").style.color = "red";
+    } else {
+      document.getElementById("notifikasiSisaKarakter").style.color = "black";
+    }
+  });
+
+  document.getElementById("inputNama").addEventListener("focus", function () {
+    console.log("inputNama: focus");
+    document.getElementById("notifikasiSisaKarakter").style.visibility =
+      "visible";
+  });
+
+  document.getElementById("inputNama").addEventListener("blur", function () {
+    console.log("inputNama: blur");
+    document.getElementById("notifikasiSisaKarakter").style.visibility =
+      "hidden";
+  });
+
+  document
+    .getElementById("inputCaptcha")
+    .addEventListener("change", function () {
+      console.log("inputCaptcha: change");
+
+      const inputCaptcha = document.getElementById("inputCaptcha").value;
+      const submitButtonStatus = document.getElementById("submitButton");
+
+      if (inputCaptcha === "PRNU") {
+        submitButtonStatus.removeAttribute("disabled");
+      } else {
+        submitButtonStatus.setAttribute("disabled", "");
+      }
+    });
+
+  document.getElementById("inputCopy").addEventListener("copy", function () {
+    alert("Anda telah men-copy sesuatu...");
+  });
+  
+  document.getElementById('inputPaste').addEventListener('paste', function(){
+    alert('Anda telah men-paste sesuatu...')
+  })
 });
