1c08224029b8fb34d56b58eecc2b7de31effaf8e
artoria_pendragon <207105651+rizzman03@users.noreply.github.com>
2025-11-10 08:21:00 +0700

    add new todoapps project

--- IDCamp2025/Tahap1/Frontend_Pemula/event/js/form2.js
+++ IDCamp2025/Tahap1/Frontend_Pemula/event/js/form2.js
@@ -1,4 +1,25 @@
+//fieldName
 document.addEventListener("DOMContentLoaded", function () {
   const inputMaxLengthOnLoad = document.getElementById("inputNama").maxLength;
   document.getElementById("sisaKarakter").innerText = inputMaxLengthOnLoad;
+
+  //Sisa karkter di konsol
+  document.getElementById("inputNama").addEventListener("input", function () {
+    const jumlahKarakterDiketik =
+      document.getElementById("inputNama").value.length;
+    const jumlahMaksimalKarakter =
+      document.getElementById("inputNama").maxLength;
+
+    console.log("jumlahKarakterDiketik: ", jumlahKarakterDiketik);
+    console.log("jumlahMaksimalKarakter: ", jumlahMaksimalKarakter);
+    const sisaKarakter = jumlahMaksimalKarakter - jumlahKarakterDiketik;
+    document.getElementById("sisaKarakter").innerText = sisaKarakter.toString();
+
+    //Sisa Karakter di fieldName
+    document.getElementById("inputNama").addEventListener("focus", function () {
+      console.log("inputNama: focus");
+      document.getElementById("notifikasiSisaKarakter").style.visibility =
+        "visible";
+    });
+  });
 });
